edbdd78c1dff1ebbb8bc6df033e82203a5412665
Graham Baradoy <graham@baradoy.ca>
2025-09-30 11:17:20 -0700

    Add SPage WebComponent

---
+++ lib/octantis_web/live/components/polaris_wc/s_page.ex
@@ -0,0 +1,139 @@
+defmodule OctantisWeb.Components.PolarisWC.SPage do
+  @moduledoc """
+  Use `s-page` as the main container for placing content in your app. Page comes with preset layouts and automatically adds spacing between elements.
+
+   ## Example
+
+  ```elixir
+  <.s_page heading="Products">
+    <.s_section>
+      <.s_text>Hello World</.s_text>
+    </.s_section>
+  </.s_page>
+
+  ## See
+  - https://shopify.dev/docs/api/app-home/polaris-web-components/structure/page
+  ```
+
+  """
+
+  use OctantisWeb.Core, :web_component
+
+  import OctantisWeb.Components.PolarisWC.SBox
+  import OctantisWeb.Components.PolarisWC.SButton
+  import OctantisWeb.Components.PolarisWC.SGrid
+  import OctantisWeb.Components.PolarisWC.SLink
+  import OctantisWeb.Components.PolarisWC.SSection
+  import OctantisWeb.Components.PolarisWC.SStack
+
+  @doc @moduledoc
+
+  ## Properties
+
+  s_attr :connected_callback, :event
+
+  s_attr :disconnected_callback, :event
+
+  s_attr :heading, :string,
+    doc: """
+    The main page heading
+    """
+
+  s_attr :inline_size, :string,
+    doc: """
+      "small" | "base" | "large"
+
+      The inline size of the page
+
+      - `base` corresponds to a set default inline size
+      - `large` full width with whitespace
+    """
+
+  ## Slots
+
+  slot :aside,
+    doc: """
+    HTMLElement
+
+    The content to display in the aside section of the page.
+
+    This slot is only rendered when `inlineSize` is "base".
+    """ do
+    attr :as, :atom, values: [:section]
+  end
+
+  slot :breadcrumb_action,
+    doc: """
+    Navigations back actions for the page.
+
+    Only accepts `Link` components.
+    """
+
+  slot :primary_action,
+    doc: """
+    The primary action for the page.
+
+    Only accepts a single `Button` component with a `variant` of `primary`.
+    """
+
+  slot :secondary_action,
+    doc: """
+    Secondary actions for the page.
+
+    Only accepts `ButtonGroup` and `Button` components with a `variant` of `secondary` or `auto`.
+    """
+
+  attr :rest, :global
+
+  slot :inner_block
+
+  def s_page(assigns) do
+    assigns = assigns |> assign_s_attrs()
+
+    ~H"""
+    <s-page {@s_attrs} {@rest}>
+      {render_slot(@inner_block)}
+      <.s_link :for={link <- @breadcrumb_action} {link} slot="breadcrumb-actions">
+        {render_slot(link)}
+      </.s_link>
+      <.s_button :for={button <- @primary_action} {button} slot="primary-action" variant="primary">
+        {render_slot(button)}
+      </.s_button>
+      <.s_button
+        :for={button <- @secondary_action}
+        {button}
+        slot="secondary-action"
+        variant="secondary"
+      >
+        {render_slot(button)}
+      </.s_button>
+      <.aside_as :for={aside <- @aside} slot="aside" {aside}>{render_slot(aside)}</.aside_as>
+    </s-page>
+    """
+  end
+
+  @doc """
+  As
+  A helper for setting the underlaying element of a component
+  """
+
+  attr :as, :atom,
+    values: [:section, :stack, :box, :grid],
+    default: :section
+
+  attr :rest, :global
+
+  slot :inner_block, required: true
+
+  def aside_as(%{as: :section} = assigns),
+    do: ~H"<.s_section {@rest}>{render_slot(@inner_block)}</.s_section>"
+
+  def aside_as(%{as: :stack} = assigns),
+    do: ~H"<.s_stack {@rest}>{render_slot(@inner_block)}</.s_stack>"
+
+  def aside_as(%{as: :box} = assigns),
+    do: ~H"<.s_box {@rest}>{render_slot(@inner_block)}</.s_box>"
+
+  def aside_as(%{as: :grid} = assigns),
+    do: ~H"<.s_grid {@rest}>{render_slot(@inner_block)}</.s_grid>"
+end
--- storybook/polaris_wc/feedback/s_banner.story.exs
+++ storybook/polaris_wc/feedback/s_banner.story.exs
@@ -3,12 +3,14 @@ defmodule Storybook.PolarisWC.SBannerStory do
 
   def function, do: &OctantisWeb.Components.Polaris.s_banner/1
 
-  def imports,
-    do: [
+  def imports do
+    [
       {OctantisWeb.Components.Head, stylesheet: 1},
       {OctantisWeb.Components.Head, javascript: 1},
-      {OctantisWeb.Components.PolarisWC.SSection, s_section: 1}
+      {OctantisWeb.Components.Polaris, s_page: 1},
+      {OctantisWeb.Components.Polaris, s_section: 1}
     ]
+  end
 
   def variations do
     [
@@ -22,7 +24,14 @@ defmodule Storybook.PolarisWC.SBannerStory do
             ],
             attributes: %{heading: "Order archived", tone: "info", dismissible: true}
           }
-        ]
+        ],
+        template: """
+        <.stylesheet psb-code-hidden/>
+        <.javascript psb-code-hidden/>
+        <.s_page >
+          <.psb-variation-group/>
+        </.s_page>
+        """
       },
       %VariationGroup{
         id: :secondary_action,
@@ -36,7 +45,14 @@ defmodule Storybook.PolarisWC.SBannerStory do
             ],
             attributes: %{heading: "Order archived", tone: "info", dismissible: true}
           }
-        ]
+        ],
+        template: """
+        <.stylesheet psb-code-hidden/>
+        <.javascript psb-code-hidden/>
+        <.s_page >
+          <.psb-variation-group/>
+        </.s_page>
+        """
       },
       %VariationGroup{
         id: :in_section,
@@ -50,12 +66,7 @@ defmodule Storybook.PolarisWC.SBannerStory do
             ],
             attributes: %{heading: "Order archived", tone: "info", dismissible: true}
           }
-        ],
-        template: """
-        <.s_section>
-          <.psb-variation-group/>
-        </.s_section>
-        """
+        ]
       }
     ]
   end
